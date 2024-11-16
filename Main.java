0fbf9b78d7e6f4cbcaac25de3bbc88e839d5bca0
Nanana776 <nanabudaghyan06@gmail.com>
2024-11-16 19:16:53 +0400

    better sqrt eval

--- Main.java
+++ Main.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 class Main{
     public static void main(String[]args){
         Scanner input= new Scanner(System.in);
-        System.out.println("Enter first value:");
-        double val1=input.nextDouble();
-        System.out.println("Enter second value:");
-        double val2=input.nextDouble();
-        input.nextLine();
         System.out.println("Enter operation (+, -, *, /, ^, sqrt):");
         String val3=input.nextLine();
+        double val1=0, val2=0;
+        if (val3=="sqrt"){
+            System.out.println("Enter value:");
+         val1=input.nextDouble();
+        }
+        else{
+            System.out.println("Enter first value:");
+         val1=input.nextDouble();
+
+        System.out.println("Enter second value:");
+         val2=input.nextDouble();
+        }
+        
+        
     input.close();
 
     Calculator calculator = new Calculator();
